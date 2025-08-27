2b05a242506d7c9077c2636824606867418b72d2
Hazique7 <haziahmed16@gmail.com>
2025-08-27 20:29:27 +0500

    feat: add google site verification meta tag

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,31 @@
+// app/layout.js
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "SIX(mp3)",
-  description: "Youtube to mp3 APP developed by Hazique Ahmed Khaan",
+  title: "SIX(mp3)",
+  description: "Youtube to mp3 APP developed by Hazique Ahmed Khaan",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
-  );
+  return (
+    <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="DPiDqaUTmVkrmbP6drq_jAa7F8EUMT6XBPOiiBMJ3BI" />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
 }
